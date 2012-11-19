4f03f237499bc43e7ab714d17f7d19ba8781d645
Alexander Reelsen <alexander@reelsen.net>
2012-11-19 09:49:52 +0100

    Added timer metric to the job execution

--- src/main/java/de/spinscale/dropwizard/jobs/Job.java
+++ src/main/java/de/spinscale/dropwizard/jobs/Job.java
@@ -1,13 +1,28 @@
 package de.spinscale.dropwizard.jobs;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public abstract class Job implements org.quartz.Job {
 
+    private final Timer timer;
+
+    public Job() {
+        timer = Metrics.defaultRegistry().newTimer(getClass(), getClass().getName());
+    }
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        doJob();
+        TimerContext timerContext = timer.time();
+        try {
+            doJob();
+        } finally {
+            timerContext.stop();
+        }
     }
 
     public abstract void doJob();
