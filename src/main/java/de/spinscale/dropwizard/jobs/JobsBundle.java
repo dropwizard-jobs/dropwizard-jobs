b605d42fea7a9ee668fec070dd3f6ba399dc3a33
Alexander Reelsen <alexander@reelsen.net>
2012-12-20 15:40:06 +0100

    Updated to dropwizard 0.6.1

--- src/main/java/de/spinscale/dropwizard/jobs/JobsBundle.java
+++ src/main/java/de/spinscale/dropwizard/jobs/JobsBundle.java
@@ -1,9 +1,14 @@
 package de.spinscale.dropwizard.jobs;
 
 import com.yammer.dropwizard.Bundle;
+import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 
-public class JobsBundle extends Bundle {
+public class JobsBundle implements Bundle {
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+    }
 
     @Override
     public void run(Environment environment) {
