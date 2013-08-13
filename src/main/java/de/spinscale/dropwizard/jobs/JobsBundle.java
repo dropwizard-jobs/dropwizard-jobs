d2d0666df8b69dcdeee728945c6d3cdea62da06b
Simon Curd <simoncurd@gmail.com>
2013-08-13 14:49:50 +0100

    added ability to configure the package name that is scanned for jobs to run

--- src/main/java/de/spinscale/dropwizard/jobs/JobsBundle.java
+++ src/main/java/de/spinscale/dropwizard/jobs/JobsBundle.java
@@ -6,13 +6,29 @@ import com.yammer.dropwizard.config.Environment;
 
 public class JobsBundle implements Bundle {
 
+	private String scanURL = null;
+	
     @Override
     public void initialize(Bootstrap<?> bootstrap) {
+    }
+    
+    public JobsBundle() {
+    }
+    
+    public JobsBundle(String scanURL) {
+    	this.scanURL = scanURL;
     }
 
     @Override
     public void run(Environment environment) {
-        environment.manage(new JobManager());
+    	if (scanURL != null)
+    	{
+    		environment.manage(new JobManager(scanURL));
+    	}
+    	else
+    	{
+    		environment.manage(new JobManager());
+    	}
     }
 
 }
