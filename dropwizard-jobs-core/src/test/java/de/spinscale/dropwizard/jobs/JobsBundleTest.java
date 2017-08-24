99ecd16a6f14130f558da86cd5fd5fcde185fe66
Derk Dukker <derk.dukker@gmail.com>
2017-08-24 14:58:54 +0200

    Updated to Dropwizard 1.1.3 and added tests for JobsBundle (#62)
    * Added tests for JobsBundle
    
    * Updated to dropwizard 1.1.3

--- dropwizard-jobs-core/src/test/java/de/spinscale/dropwizard/jobs/JobsBundleTest.java
+++ dropwizard-jobs-core/src/test/java/de/spinscale/dropwizard/jobs/JobsBundleTest.java
@@ -1,12 +1,17 @@
 package de.spinscale.dropwizard.jobs;
 
+import com.codahale.metrics.SharedMetricRegistries;
+import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import static de.spinscale.dropwizard.jobs.Job.DROPWIZARD_JOBS_KEY;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.Mockito.mock;
@@ -21,13 +26,27 @@ public class JobsBundleTest {
     @Test
     public void assertJobsBundleIsWorking() throws Exception {
         when(environment.lifecycle()).thenReturn(applicationContext);
-        new JobsBundle().run(new MyConfiguration(), environment);
+        final JobsBundle jobsBundle = new JobsBundle();
+        jobsBundle.run(new MyConfiguration(), environment);
 
         final ArgumentCaptor<JobManager> captor = ArgumentCaptor.forClass(JobManager.class);
         verify(applicationContext).manage(captor.capture());
 
         JobManager jobManager = captor.getValue();
         assertThat(jobManager, is(notNullValue()));
+        assertThat(jobsBundle.getScheduler(), is(jobManager.getScheduler()));
+    }
+
+    @Test
+    public void should_add_dropwizard_jobs_to_metrics_registry() throws Exception {
+        new JobsBundle().initialize(new Bootstrap<>(new Application<MyConfiguration>() {
+            @Override
+            public void run(MyConfiguration myConfiguration, Environment environment) throws Exception {
+
+            }
+        }));
+
+        assertThat(SharedMetricRegistries.names(), hasItem(DROPWIZARD_JOBS_KEY));
     }
 
     private static class MyConfiguration extends Configuration implements JobConfiguration {}
