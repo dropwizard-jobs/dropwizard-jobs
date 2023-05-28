2e5fcd399f6cbbebf2912ad71cef7c16de0e3609
Hakan Dilek <hakandilek@gmail.com>
2023-05-28 13:36:19 +0200

    refactor: introduce JobFilters as stream

--- dropwizard-jobs-core/src/main/java/io/dropwizard/jobs/DropwizardJobFactory.java
+++ dropwizard-jobs-core/src/main/java/io/dropwizard/jobs/DropwizardJobFactory.java
@@ -3,36 +3,25 @@ package io.dropwizard.jobs;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
 import org.quartz.spi.JobFactory;
 import org.quartz.spi.TriggerFiredBundle;
-import org.quartz.utils.Key;
-
-import java.util.Objects;
 
 class DropwizardJobFactory implements JobFactory {
 
-    private final Job[] jobs;
+    private final JobFilters jobs;
 
-    DropwizardJobFactory(Job ... jobs) {
+    DropwizardJobFactory(JobFilters jobs) {
         this.jobs = jobs;
     }
 
     @Override
-    public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) throws SchedulerException {
+    public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler) {
         JobDetail jobDetail = bundle.getJobDetail();
+        Class<? extends org.quartz.Job> jobClass = jobDetail.getJobClass();
         JobKey jobKey = jobDetail.getKey();
+        String groupName = jobKey.getGroup();
 
-        for (Job job : jobs) {
-            if (job.getClass().equals(jobDetail.getJobClass()) && equalGroupName(job, jobKey)) {
-                return job;
-            }
-        }
-        return null;
+        return jobs.findWith(jobClass, groupName).orElse(null);
     }
 
-    private boolean equalGroupName(final Job job, final JobKey quartzJobKey) {
-        return Key.DEFAULT_GROUP.equals(quartzJobKey.getGroup()) && job.getGroupName() == null
-            || Objects.equals(job.getGroupName(), quartzJobKey.getGroup());
-    }
 }
---
+++ dropwizard-jobs-core/src/main/java/io/dropwizard/jobs/JobFilters.java
@@ -0,0 +1,53 @@
+package io.dropwizard.jobs;
+
+import io.dropwizard.jobs.annotations.Every;
+import io.dropwizard.jobs.annotations.On;
+import io.dropwizard.jobs.annotations.OnApplicationStart;
+import io.dropwizard.jobs.annotations.OnApplicationStop;
+import org.quartz.utils.Key;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class JobFilters {
+    private final Job[] jobs;
+
+    public JobFilters(Job[] jobs) {
+
+        this.jobs = jobs;
+    }
+
+    public Stream<Job> allOnApplicationStop() {
+        return Arrays.stream(jobs)
+                .filter(job -> job.getClass().isAnnotationPresent(OnApplicationStop.class));
+    }
+
+    public Stream<Job> allOnCron() {
+        return Arrays.stream(this.jobs)
+                .filter(job -> job.getClass().isAnnotationPresent(On.class));
+    }
+
+    public Stream<Job> allEvery() {
+        return Arrays.stream(this.jobs)
+                .filter(job -> job.getClass().isAnnotationPresent(Every.class));
+    }
+
+    public Stream<Job> allOnApplicationStart() {
+        return Arrays.stream(this.jobs)
+                .filter(job -> job.getClass().isAnnotationPresent(OnApplicationStart.class));
+    }
+
+    public Optional<Job> findWith(Class<? extends org.quartz.Job> jobClass, String groupName) {
+        return Arrays.stream(this.jobs)
+                .filter(job -> job.getClass().equals(jobClass) && equalGroupName(job, groupName))
+                .findFirst();
+    }
+
+    private boolean equalGroupName(final Job job, final String groupName) {
+        String jobGroupName = job.getGroupName();
+        return Key.DEFAULT_GROUP.equals(groupName) && jobGroupName == null
+                || Objects.equals(jobGroupName, groupName);
+    }
+}
