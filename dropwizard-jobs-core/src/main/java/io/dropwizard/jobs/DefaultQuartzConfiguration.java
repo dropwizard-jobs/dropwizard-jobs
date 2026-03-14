baf8b7132d8e647206765a33f6808050f7812ab7
Hakan Dilek <hakandilek@gmail.com>
2026-03-14 17:13:42 +0100

    refactor: make DefaultQuartzConfiguration immutable and rename field for clarity
    - Rename persistenceConfiguration to defaultQuartzProperties (fixes naming issue)
    - Use Map.of() to create immutable map (thread-safe, prevents mutation)
    - Update test to use HashMap copy constructor instead of casting/cloning
    - Resolves thread safety, configuration mutation, and singleton pattern issues

--- dropwizard-jobs-core/src/main/java/io/dropwizard/jobs/DefaultQuartzConfiguration.java
+++ dropwizard-jobs-core/src/main/java/io/dropwizard/jobs/DefaultQuartzConfiguration.java
@@ -1,21 +1,18 @@
 package io.dropwizard.jobs;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class DefaultQuartzConfiguration {
-    private static Map<String, String> persistenceConfiguration;
-    static {
-        persistenceConfiguration = new HashMap<String, String>();
-        persistenceConfiguration.put("org.quartz.scheduler.instanceName", "scheduler");
-        persistenceConfiguration.put("org.quartz.scheduler.instanceId", "AUTO");
-        persistenceConfiguration.put("org.quartz.scheduler.skipUpdateCheck", "true");
-        persistenceConfiguration.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
-        persistenceConfiguration.put("org.quartz.threadPool.threadCount", "10");
-        persistenceConfiguration.put("org.quartz.threadPool.threadPriority", "5");
-        persistenceConfiguration.put("org.quartz.jobStore.misfireThreshold", "60000");
-    }
+    private static final Map<String, String> defaultQuartzProperties = Map.of(
+        "org.quartz.scheduler.instanceName", "scheduler",
+        "org.quartz.scheduler.instanceId", "AUTO",
+        "org.quartz.scheduler.skipUpdateCheck", "true",
+        "org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool",
+        "org.quartz.threadPool.threadCount", "10",
+        "org.quartz.threadPool.threadPriority", "5",
+        "org.quartz.jobStore.misfireThreshold", "60000");
+
     public static Map<String, String> get() {
-        return persistenceConfiguration;
+        return defaultQuartzProperties;
     }
 }
